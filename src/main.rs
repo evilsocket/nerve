8982022e62b941d4ce4d5b5a3ae133e621232dd7
Simone Margaritelli <evilsocket@gmail.com>
2024-06-13 11:14:14 +0200

    misc: small fix or general refactoring i did not bother commenting

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,8 @@ mod cli;
 async fn main() -> Result<()> {
     let args = cli::Args::parse();
 
+    // create generator
     let gen_options = args.to_generator_options()?;
-
-    let mut tasklet: Tasklet = Tasklet::from_path(&args.tasklet, &args.define)?;
-
-    if tasklet.prompt.is_none() {
-        tasklet.prompt = Some(if let Some(prompt) = &args.prompt {
-            prompt.to_string()
-        } else {
-            cli::get_user_input("enter task> ")
-        });
-    }
-    let task = Box::new(tasklet);
-
-    println!("{}: {}", "task".bold(), task.to_prompt()?.trim().yellow());
-
     let generator = generator::factory(
         &gen_options.type_name,
         &gen_options.host,
@@ -47,6 +34,23 @@ async fn main() -> Result<()> {
         gen_options.port.to_string().dimmed()
     );
 
+    // read and create the tasklet
+    let mut tasklet: Tasklet = Tasklet::from_path(&args.tasklet, &args.define)?;
+    // if the tasklet doesn't provide a prompt
+    if tasklet.prompt.is_none() {
+        tasklet.prompt = Some(if let Some(prompt) = &args.prompt {
+            // if passed by command line
+            prompt.to_string()
+        } else {
+            // ask the user
+            cli::get_user_input("enter task> ")
+        });
+    }
+    let task = Box::new(tasklet);
+
+    println!("{}: {}", "task".bold(), task.to_prompt()?.trim().yellow());
+
+    // create the agent given the generator, task and a set of options
     let mut agent = Agent::new(generator, task, args.to_agent_options())?;
 
     println!(
@@ -55,7 +59,9 @@ async fn main() -> Result<()> {
         agent.state().used_namespaces().join(", ")
     );
 
+    // keep going until the task is complete or a fatal error is reached
     while !agent.get_state().is_complete() {
+        // next step
         if let Err(error) = agent.step().await {
             println!("{}", error.to_string().bold().red());
             return Err(error);
