5ebed66bc51b2f41400600dc2a17ae53362cf216
evilsocket <evilsocket@gmail.com>
2025-03-28 12:49:41 +0100

    fix: minor improvements of the nerve agents output

--- nerve/cli/agents.py
+++ nerve/cli/agents.py
@@ -3,7 +3,6 @@ import pathlib
 import typing as t
 
 import typer
-from loguru import logger
 from termcolor import colored
 
 import nerve
@@ -29,31 +28,25 @@ def agents(
         typer.Argument(help="Path to the agent or workflow to create"),
     ] = DEFAULT_AGENTS_LOAD_PATH,
 ) -> None:
-    print(f"🧠 nerve v{nerve.__version__}")
+    print(f"🧠 nerve v{nerve.__version__}\n")
 
     asyncio.run(show_agents(path))
 
 
 async def show_agents(path: pathlib.Path) -> None:
-    if not path.exists():
-        logger.error(f"Path {path} does not exist")
-        raise typer.Exit(1)
-
-    if not path.is_dir():
-        logger.error(f"Path {path} is not a directory")
-        raise typer.Exit(1)
-
-    print()
-    print(f"📁 {path.absolute()}")
-
     anything = False
-    for item in path.iterdir():
-        if Workflow.is_workflow(item):
-            print(f"   {item.name} " + colored("<workflow>", "blue"))
-            anything = True
-        elif Configuration.is_agent_config(item):
-            print(f"   {item.name} " + colored("<agent>", "green"))
-            anything = True
+
+    if path.exists() and path.is_dir():
+        print()
+        print(f"📁 {path.absolute()}")
+
+        for item in path.iterdir():
+            if Workflow.is_workflow(item):
+                print(f"   {item.name} " + colored("<workflow>", "blue"))
+                anything = True
+            elif Configuration.is_agent_config(item):
+                print(f"   {item.name} " + colored("<agent>", "green"))
+                anything = True
 
     if not anything:
-        print(colored("no agents or workflows found", "light_grey"))
+        print(colored(f"No agents or workflows found in {path}", "light_grey"))
